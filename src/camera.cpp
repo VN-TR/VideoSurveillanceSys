b2ef59d777f7291e3ab7bc78d83f80eee907a0cb
ybc <bcyang@visionnav.com>
2020-04-26 16:28:27 +0800

    画skeleton的图

--- src/camera.cpp
+++ src/camera.cpp
@@ -106,14 +106,14 @@ namespace VisionMonitor
 				skeleton_estimation(image_);
 				cout << frame_index_ << endl;
 
-				
-				AI_result_.clear();
-				AI_result_ = object_detection_.DL_Detector(image_, skeleton_image_, display_image);
-				if (AI_result_.size() != 0)
-				{
-					image_ = AI_result_.back().itemImage;
-					AI_result_.clear();
-				}
+				display_image = skeleton_image_;
+				//AI_result_.clear();
+				//AI_result_ = object_detection_.DL_Detector(image_, skeleton_image_, display_image);
+				//if (AI_result_.size() != 0)
+				//{
+				//	image_ = AI_result_.back().itemImage;
+				//	AI_result_.clear();
+				//}
 				
 			}
 		}
@@ -268,6 +268,9 @@ namespace VisionMonitor
 	void Camera::skeleton_estimation(const Mat input_image)
 	{
 		Mat skeleton_input_image = input_image;
+		resize(skeleton_input_image, skeleton_input_image,
+			Size(skeleton_input_image.cols / param_.skeleton_desample_rate,
+				skeleton_input_image.rows / param_.skeleton_desample_rate));
 		auto datumProcessed = opWrapper.emplaceAndPop(skeleton_input_image);
 		if (datumProcessed != nullptr)
 		{
@@ -293,11 +296,253 @@ namespace VisionMonitor
 	{
 		Mat outputimage = input_image;
 
-		for (int i = 0; i < skeleton_point_.size(); i += 3)
+		int humanCount = skeletonPoint.size() / 75;
+		int lineWidth = 6;
+		int pointWidth = 8;
+		Timer mytime;
+		mytime.tic();
+		for (auto i = 0; i < humanCount; i++)
 		{
-			Point pt((int)skeleton_point_[i], (int)skeleton_point_[i + 1]);
-			circle(outputimage, pt, 6, Scalar(255, 0, 0), -1);
+			//»­½Úµã£»
+			//Í·²¿
+			//0-±Ç×Ó
+			Point pt0(0,0);
+			if (skeleton_point_[i * 75] != 0)
+			{
+				pt0 = Point((int)skeleton_point_[i * 75] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt0, pointWidth, Scalar(74, 38, 252), -1);
+			}
+			//1-Neck
+			Point pt1(0, 0);
+			if (skeleton_point_[i * 75 + 1*3] != 0)
+			{
+				pt1 = Point((int)skeleton_point_[i * 75 + 1 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 1 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt1, pointWidth, Scalar(31, 42, 244), -1);
+			}
+			//15-REye
+			Point pt15(0, 0);
+			if (skeleton_point_[i * 75 + 15 * 3] != 0)
+			{
+				pt15 = Point((int)skeleton_point_[i * 75 + 15 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 15 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt15, pointWidth, Scalar(171, 26, 246), -1);
+			}
+			//16-LEye
+			Point pt16(0, 0);
+			if (skeleton_point_[i * 75 + 16 * 3] != 0)
+			{
+				pt16 = Point((int)skeleton_point_[i * 75 + 16 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 16 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt16, pointWidth, Scalar(170, 16, 74), -1);
+			}
+			//17-REar
+			Point pt17(0, 0);
+			if (skeleton_point_[i * 75 + 17 * 3] != 0)
+			{
+				pt17 = Point((int)skeleton_point_[i * 75 + 17 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 17 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt17, pointWidth, Scalar(171, 26, 246), -1);
+			}			
+			//18-LEar
+			Point pt18(0, 0);
+			if (skeleton_point_[i * 75 + 18 * 3] != 0)
+			{
+				pt18 = Point((int)skeleton_point_[i * 75 + 18 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 18 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt18, pointWidth, Scalar(170, 16, 74), -1);
+			}
+			if (pt0.x != 0 && pt1.x != 0)
+			{
+				line(outputimage, pt0, pt1, Scalar(74, 38, 252), lineWidth);
+			}
+			if (pt0.x != 0 && pt15.x != 0)
+			{
+				line(outputimage, pt0, pt15, Scalar(171, 26, 246), lineWidth);
+			}
+			if (pt0.x != 0 && pt16.x != 0)
+			{
+				line(outputimage, pt0, pt16, Scalar(170, 16, 74), lineWidth);
+			}
+			if (pt17.x != 0 && pt15.x != 0)
+			{
+				line(outputimage, pt17, pt15, Scalar(171, 26, 246), lineWidth);
+			}
+			if (pt18.x != 0 && pt16.x != 0)
+			{
+				line(outputimage, pt18, pt16, Scalar(170, 16, 74), lineWidth);
+			}
+
+			//ÓÒ±Û
+			//2-RShoulder
+			Point pt2(0, 0);
+			if (skeleton_point_[i * 75 + 2 * 3] != 0)
+			{
+				pt2 = Point((int)skeleton_point_[i * 75 + 2 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 2 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt2, pointWidth, Scalar(1, 126, 242), -1);
+			}
+			//3-RElbow
+			Point pt3(0, 0);
+			if (skeleton_point_[i * 75 + 3 * 3] != 0)
+			{
+				pt3 = Point((int)skeleton_point_[i * 75 + 3 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 3 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt3, pointWidth, Scalar(0, 203, 253), -1);
+			}
+			//4-RWrist
+			Point pt4(0, 0);
+			if (skeleton_point_[i * 75 + 4 * 3] != 0)
+			{
+				pt4 = Point((int)skeleton_point_[i * 75 + 4 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 4 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt4, pointWidth, Scalar(67, 238, 245), -1);
+			}
+			if (pt1.x != 0 && pt2.x != 0)
+			{
+				line(outputimage, pt1, pt2, Scalar(1, 126, 242), lineWidth);
+			}
+			if (pt2.x != 0 && pt3.x != 0)
+			{
+				line(outputimage, pt2, pt3, Scalar(0, 203, 253), lineWidth);
+			}
+			if (pt3.x != 0 && pt4.x != 0)
+			{
+				line(outputimage, pt3, pt4, Scalar(67, 238, 245), lineWidth);
+			}
+
+			//×ó±Û
+			//5-LShoulder
+			Point pt5(0, 0);
+			if (skeleton_point_[i * 75 + 5 * 3] != 0)
+			{
+				pt5 = Point((int)skeleton_point_[i * 75 + 5 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 5 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt5, pointWidth, Scalar(0, 253, 184), -1);
+			}
+			//6-LElbow
+			Point pt6(0, 0);
+			if (skeleton_point_[i * 75 + 6 * 3] != 0)
+			{
+				pt6 = Point((int)skeleton_point_[i * 75 + 6 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 6 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt6, pointWidth, Scalar(80, 234, 89), -1);
+			}
+			//7-LWrist
+			Point pt7(0, 0);
+			if (skeleton_point_[i * 75 + 7 * 3] != 0)
+			{
+				pt7 = Point((int)skeleton_point_[i * 75 + 7 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 7 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt7, pointWidth, Scalar(82, 234, 93), -1);
+			}
+			if (pt1.x != 0 && pt5.x != 0)
+			{
+				line(outputimage, pt1, pt5, Scalar(0, 253, 184), lineWidth);
+			}
+			if (pt5.x != 0 && pt6.x != 0)
+			{
+				line(outputimage, pt5, pt6, Scalar(80, 234, 89), lineWidth);
+			}
+			if (pt6.x != 0 && pt7.x != 0)
+			{
+				line(outputimage, pt6, pt7, Scalar(82, 234, 93), lineWidth);
+			}
+
+			//8-MidHip
+			Point pt8(0, 0);
+			if (skeleton_point_[i * 75 + 8 * 3] != 0)
+			{
+				pt8 = Point((int)skeleton_point_[i * 75 + 8 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 8 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt8, pointWidth, Scalar(31, 42, 244), -1);
+			}
+			if (pt8.x != 0 && pt1.x != 0)
+			{
+				line(outputimage, pt8, pt1, Scalar(31, 42, 244), lineWidth);
+			}
+			//ÓÒÍÈ
+			//9-RHip
+			Point pt9(0, 0);
+			if (skeleton_point_[i * 75 + 9 * 3] != 0)
+			{
+				pt9 = Point((int)skeleton_point_[i * 75 + 9 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 9 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt9, pointWidth, Scalar(76, 252, 30), -1);
+			}
+			//10-RKnee
+			Point pt10(0, 0);
+			if (skeleton_point_[i * 75 + 10 * 3] != 0)
+			{
+				pt10 = Point((int)skeleton_point_[i * 75 + 10 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 10 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt10, pointWidth, Scalar(150, 195, 47), -1);
+			}
+			//11-RAnkle
+			Point pt11(0, 0);
+			if (skeleton_point_[i * 75 + 11 * 3] != 0)
+			{
+				pt11 = Point((int)skeleton_point_[i * 75 + 11 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 11 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt11, pointWidth, Scalar(208, 212, 44), -1);
+			}
+			if (pt8.x != 0 && pt9.x != 0)
+			{
+				line(outputimage, pt8, pt9, Scalar(76, 252, 30), lineWidth);
+			}
+			if (pt9.x != 0 && pt10.x != 0)
+			{
+				line(outputimage, pt9, pt10, Scalar(150, 195, 47), lineWidth);
+			}
+			if (pt10.x != 0 && pt11.x != 0)
+			{
+				line(outputimage, pt10, pt11, Scalar(208, 212, 44), lineWidth);
+			}
+			//×óÍÈ
+			//12-RHip
+			Point pt12(0, 0);
+			if (skeleton_point_[i * 75 + 12 * 3] != 0)
+			{
+				pt12 = Point((int)skeleton_point_[i * 75 + 12 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 12 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt12, pointWidth, Scalar(193, 150, 53), -1);
+			}
+			//13-RKnee
+			Point pt13(0, 0);
+			if (skeleton_point_[i * 75 + 13 * 3] != 0)
+			{
+				pt13 = Point((int)skeleton_point_[i * 75 + 13 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 13 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt13, pointWidth, Scalar(212, 80, 36), -1);
+			}
+			//14-RAnkle
+			Point pt14(0, 0);
+			if (skeleton_point_[i * 75 + 14 * 3] != 0)
+			{
+				pt14 = Point((int)skeleton_point_[i * 75 + 14 * 3] * param_.skeleton_desample_rate,
+					(int)skeleton_point_[i * 75 + 14 * 3 + 1] * param_.skeleton_desample_rate);
+				circle(outputimage, pt14, pointWidth, Scalar(198, 39, 45), -1);
+			}
+			if (pt8.x != 0 && pt12.x != 0)
+			{
+				line(outputimage, pt8, pt12, Scalar(193, 150, 53), lineWidth);
+			}
+			if (pt12.x != 0 && pt13.x != 0)
+			{
+				line(outputimage, pt12, pt13, Scalar(212, 80, 36), lineWidth);
+			}
+			if (pt13.x != 0 && pt14.x != 0)
+			{
+				line(outputimage, pt13, pt14, Scalar(198, 39, 45), lineWidth);
+			}
+
+			cout << "»­Í¼Ê±¼ä" << mytime.toc() << endl;
+
 		}
+
+
+
 		return outputimage;		
 	}
 
