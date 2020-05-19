bf179ac83dd5448092cc46cb6d5b98b8a6d0154d
ybc <bcyang@visionnav.com>
2020-05-19 13:27:28 +0800

    将显示图片日志变为视频

--- apps/videoSurverillanceSys.cpp
+++ apps/videoSurverillanceSys.cpp
@@ -72,16 +72,41 @@ using namespace VisionMonitor;
 //	return 0;
 //}
 //
+//int main()
+//{
+//
+//	Monitor monitor;
+//	monitor.initiate();
+//	monitor.start();
+//
+//	getchar();
+//	return 0;
+//}
 int main()
 {
+	Mat image;
+	char filename[75]; //定义数组，其中数组大小大于等于图片的个数
+	int isColor = 1;   //如果为0 ，可输出灰度图像
+	int fps = 12;      //设置输出视频的帧率
+	//int frameWidth = image.cols;   //单帧图片的宽
+	//int frameHeight = image.rows;  //单帧图片的高
 
-	Monitor monitor;
-	monitor.initiate();
-	monitor.start();
+	VideoWriter writer("video_out.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'), fps,
+		Size(1920, 1080), isColor);
 
-	getchar();
-	return 0;
+	for (unsigned int i = 0; i < 1000; i++)
+	{
+		sprintf(filename, "display/%d.jpg", i);//第二个参数是指定图片路径和图片名的一般式（image%d）
+		image = imread(filename);//导入图片
+		if (image.empty())
+		{
+			break;
+		}
+		waitKey(0);
+		writer.write(image); //
+	}
 }
+
 
 //#include <stdio.h>
 //#include <stdlib.h>
